64d40c15f56ae11f3769d0a7e43d16dc52fd644b
lucmanm <greenjamez22@gmail.com>
2023-07-05 21:40:25 +0300

    NEXTJS BLOG FULSTACK

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,34 @@
+import prisma from "@/prisma";
+import { NextResponse } from "next/server";
+
+export async function main() {
+    try {
+        await prisma.$connect();
+    } catch (err) {
+        return Error("Database is not connected")
+    }
+}
+
+export const GET =async (req:Request, res: NextResponse) => {
+    try {
+        await main();
+        const posts = await prisma.post.findMany();
+        return NextResponse.json({message: "Successfull", posts}, {status: 200})
+    } catch (err) {
+        return NextResponse.json({message: "error", err}, {status: 500})
+    }finally{
+        await prisma.$disconnect();
+    }
+}
+export const POST = async (req:Request, res: NextResponse) => {
+    try {
+        const {title, description} = await req.json();
+        await main();
+        const post = await prisma.post.create({data: {title, description}})
+        return NextResponse.json({message: "Successfully Created", post}, {status: 201})
+    } catch (err) {
+        return NextResponse.json({message: "error", err}, {status: 500})
+    }finally{
+        await prisma.$disconnect();
+    }
+}
